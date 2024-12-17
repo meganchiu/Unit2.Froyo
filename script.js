1eef02d553180e266c2b1abab73782d1f4ebae31
Megan Chiu <megan.chiu.2014@gmail.com>
2024-12-17 08:30:58 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let flavors = window.prompt("Please enter your froyo flavors below separated by comma with no spaces:")
+
+console.log(flavors)
+
+flavors.trim();
+const flavorsArr = flavors.split(",");
+console.log(flavorsArr);
+
+function toObject(flavorsArr) {
+  const flavorsObj = {};
+
+  flavorsArr.forEach(flavor => {
+    // If key does exist, add 1 to the existing value
+    if (flavorsObj[flavor]) {
+      flavorsObj[flavor] = flavorsObj[flavor] + 1;
+    } 
+    // If key doesn't exist, set the value to 1
+    else {
+      flavorsObj[flavor] = 1;
+    }
+  });
+
+  return flavorsObj;
+}
+
+console.log("object", toObject(flavorsArr));
